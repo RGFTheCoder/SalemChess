35f371734eb4752df33647ab6b5de53df4f7d4f9
WillQi <williamqipizza@gmail.com>
2018-08-03 14:39:36 -0600

    Doctor's Heal no longer affects doctors.

--- Engine/Game/Roles/Doctor.js
+++ Engine/Game/Roles/Doctor.js
@@ -12,15 +12,15 @@ class Doctor extends Role {
     passiveEffects () {
         const tileLeft = this.piece.tile.game.getTile(this.piece.tile.x - 1, this.piece.tile.y);
         const tileRight = this.piece.tile.game.getTile(this.piece.tile.x + 1, this.piece.tile.y);
-        if (tileLeft && tileLeft.piece && tileLeft.piece.isWhite === this.piece.isWhite) tileLeft.piece.addEffect(new DoctorHeal(this.piece, tileLeft.piece));
-        if (tileRight && tileRight.piece && tileRight.piece.isWhite === this.piece.isWhite) tileRight.piece.addEffect(new DoctorHeal(this.piece, tileRight.piece));
+        if (tileLeft && tileLeft.piece && tileLeft.piece.isWhite === this.piece.isWhite && !(tileLeft.piece.role && tileLeft.piece.role.name !== this.name)) tileLeft.piece.addEffect(new DoctorHeal(this.piece, tileLeft.piece));
+        if (tileRight && tileRight.piece && tileRight.piece.isWhite === this.piece.isWhite && !(tileRight.piece.role && tileRight.piece.role.name !== this.name)) tileRight.piece.addEffect(new DoctorHeal(this.piece, tileRight.piece));
         let tileUp;
         if (this.piece.isWhite) {
             tileUp = this.piece.tile.game.getTile(this.piece.tile.x, this.piece.tile.y - 1);
         } else {
             tileUp = this.piece.tile.game.getTile(this.piece.tile.x, this.piece.tile.y + 1);
         }
-        if (tileUp && tileUp.piece && tileUp.piece.isWhite === this.piece.isWhite) tileUp.piece.addEffect(new DoctorHeal(this.piece, tileUp.piece));
+        if (tileUp && tileUp.piece && tileUp.piece.isWhite === this.piece.isWhite && !(tileUp.piece.role && tileUp.piece.role.name !== this.name)) tileUp.piece.addEffect(new DoctorHeal(this.piece, tileUp.piece));
     }
 }
 
